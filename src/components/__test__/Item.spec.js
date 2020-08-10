2aac4cf441f871fb4b7c26528244a888f3c0d8d8
jackzhoumine <jackzhoumine@163.com>
2020-08-11 02:51:03 +0800

    合并脚本

--- src/components/__test__/Item.spec.js
+++ src/components/__test__/Item.spec.js
@@ -3,13 +3,11 @@
  * @Description: Item 组件测试文件
  * @Date: Tue Aug 11 2020 01:47:26
  * @Author: JackChouMine
- * @LastEditTime: 2020-08-11 02:39:05 +0800
+ * @LastEditTime: 2020-08-11 02:50:06 +0800
  * @LastEditors: JackChouMine
  */
 // 第一个参数是测试报告中的说明文字
 // 第二个参数测试函数，测试时会执行该函数，返回测试结果。
 test('sanity test', () => {
-  // 这里会输出
-  console.log('可用性测试')
   return
 })
